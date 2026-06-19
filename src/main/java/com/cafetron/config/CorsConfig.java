4f3574325bba09c5a257376e8f5e539e543546e7
Dhruv S Shah <dhruvsshah171@gmail.com>
2026-06-19 08:56:31 +0530

    trying to enable allowedOriginPatterns

--- src/main/java/com/cafetron/config/CorsConfig.java
+++ src/main/java/com/cafetron/config/CorsConfig.java
@@ -20,7 +20,7 @@ public class CorsConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOriginPatterns(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("https://cafetron-frontend-iy4p.vercel.app/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
